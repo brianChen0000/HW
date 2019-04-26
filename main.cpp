3ad8edb841cd84c7a63f48d4c098cae31b328b6e
brianChen0000 <50001902+brianChen0000@users.noreply.github.com>
2019-04-26 08:39:17 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  homework4
+//
+//  Created by Mac on 2019/3/15.
+//  Copyright © 2019年 Mac. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+    printf("Hello C!\n");
+    printf("Hello World 3A713219\n");
+    system("pause");
+    return 0 ;
+}
